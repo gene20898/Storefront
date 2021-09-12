b8d1d042fae99c5f38ed1a630ffd988bdc7fd24b
Wayu Bangkamed <gene20898@hotmail.com>
2021-09-12 21:56:58 +0700

    fix bug

--- store_frontend/bin/deploy.sh
+++ store_frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws cp --recursive --acl public-read ./dist/store-frontend s3://udacity-project-gene-s3/
+aws s3 cp --recursive --acl public-read ./dist/store-frontend s3://udacity-project-gene-s3/
 
