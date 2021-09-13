f8c2c60dca8e75ddc83a2b45fda330092857e839
วายุ บางเขม็ด <gene20898@hotmail.com>
2021-09-13 21:12:18 +0700

    Edit backend test script

--- store_backend/src/database.ts
+++ store_backend/src/database.ts
@@ -15,18 +15,19 @@ const{
 } = process.env
 
 let Client: Pool|null = null;
-console.log(`Environment : ${ENV}`);
 
-if(ENV === 'test') {
+if(ENV === 'test') {    
+    console.log("Creating test local database connection");
     Client = new Pool({
-        host: POSTGRES_HOST_TEST,
+        host: POSTGRES_HOST,
         database: POSTGRES_TEST_DB,
         user: POSTGRES_USER,
         password: POSTGRES_PASSWORD,
     })
+    console.log(Client);
 }
 if(ENV === 'dev') {
-    console.log("Creating dev Client database");
+    console.log("Creating dev local database connection");
     Client = new Pool({
         host: POSTGRES_HOST,
         port: 5432,
