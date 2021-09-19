5d9ed32a3827570fbf377b23333717d96207ff89
วายุ บางเขม็ด <gene20898@hotmail.com>
2021-09-19 17:57:18 +0700

    edit angular env

--- store_frontend/src/environments/environment.prod.ts
+++ store_frontend/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiHost: "https://storefront-env.eba-3bvrzt4c.ap-southeast-1.elasticbeanstalk.com",
+  apiHost: "https://api.saberbeagle.com",
   auth: {
     domain: "dev-35p-vy7k.us.auth0.com",
     clientId: "AYTkcAxg59khNHCxSX2bsXeNVvelUbw3",
     audience: "https://storefront-env.eba-3bvrzt4c.ap-southeast-1.elasticbeanstalk.com"
   },
   dev: {
-    serverUrl: "https://storefront-env.eba-3bvrzt4c.ap-southeast-1.elasticbeanstalk.com",
+    serverUrl: "https://api.saberbeagle.com",
   },
 };
