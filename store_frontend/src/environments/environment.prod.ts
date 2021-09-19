b6193567b807ca904aa127736ddeb9b9d4fe24bb
วายุ บางเขม็ด <gene20898@hotmail.com>
2021-09-19 12:03:09 +0700

    fix angular env

--- store_frontend/src/environments/environment.prod.ts
+++ store_frontend/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   apiHost: "http://storefront-env.eba-3bvrzt4c.ap-southeast-1.elasticbeanstalk.com",
   auth: {
     domain: "dev-35p-vy7k.us.auth0.com",
+    clientId: "AYTkcAxg59khNHCxSX2bsXeNVvelUbw3",
     audience: "http://storefront-env.eba-3bvrzt4c.ap-southeast-1.elasticbeanstalk.com"
   },
   dev: {
